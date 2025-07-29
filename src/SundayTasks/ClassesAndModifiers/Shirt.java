4c9058815b2e881e49cde439361db494ef954839
tomer-hershman <lili.hershman@gmail.com>
2025-07-29 18:07:39 +0300

    tomer- classes and modifiers, added shirt

---
+++ src/SundayTasks/ClassesAndModifiers/Shirt.java
@@ -0,0 +1,12 @@
+package SundayTasks.ClassesAndModifiers;
+
+public class Shirt {
+    public String color;
+    public int size;
+
+    public Shirt(String color, int size) {
+        this.color = color;
+        this.size = size;
+    }
+
+}
