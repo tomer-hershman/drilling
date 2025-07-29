ca4fb8436c0e42cea168deed4276000eb4dde6a3
tomer-hershman <lili.hershman@gmail.com>
2025-07-29 17:56:51 +0300

    tomer- classes and modifiers Q1

---
+++ src/SundayTasks/ClassesAndModifiers/Bed.java
@@ -0,0 +1,13 @@
+package SundayTasks.ClassesAndModifiers;
+
+public class Bed {
+    private double height;
+    private int pillows;
+    private String sheetColor;
+
+    public Bed(double height, int pillows, String sheetColor) {
+        this.height = height;
+        this.pillows = pillows;
+        this.sheetColor = sheetColor;
+    }
+}
