2b8d9f2176f4464624468b11c8db8fc99f35efbf
tomer-hershman <lili.hershman@gmail.com>
2025-07-29 18:11:57 +0300

    tomer- classes and modifiers, added closet, ctrl+alt+L

--- src/SundayTasks/ClassesAndModifiers/Bed.java
+++ src/SundayTasks/ClassesAndModifiers/Bed.java
@@ -12,21 +12,26 @@ public class Bed {
         this.sheetColor = sheetColor;
         this.price = price;
     }
-    public Bed (double hight, String sheetColor, double price) {
+
+    public Bed(double hight, String sheetColor, double price) {
         this.height = height;
         this.pillows = 1;
         this.sheetColor = sheetColor;
         this.price = price;
     }
-    public double getHeight(){
+
+    public double getHeight() {
         return height;
     }
+
     public int getPillows() {
         return pillows;
     }
+
     public String getSheetColor() {
         return sheetColor;
     }
+
     public double getPrice() {
         return price;
     }
---
+++ src/SundayTasks/ClassesAndModifiers/Closet.java
@@ -0,0 +1,13 @@
+package SundayTasks.ClassesAndModifiers;
+
+public class Closet {
+    private Shirt[] shirts;
+
+    public Closet(Shirt[] shirts) {
+        this.shirts = shirts;
+    }
+
+    public Shirt[] getShirts() {
+        return shirts;
+    }
+}
