0884b69d9a25298f0643938c516589ca9a8aa24c
tomer-hershman <lili.hershman@gmail.com>
2025-07-29 18:29:50 +0300

    tomer- classes and modifiers, added assistent class

---
+++ src/SundayTasks/ClassesAndModifiers/ShirtListMerger.java
@@ -0,0 +1,12 @@
+package SundayTasks.ClassesAndModifiers;
+
+public class ShirtListMerger {
+    protected Shirt[] addShirtToList(Shirt[] shirtList, Shirt shirt) {
+        Shirt[] newShirtList = new Shirt[shirtList.length + 1];
+        for (int i = 0; i < shirtList.length; i++) {
+            newShirtList[i] = shirtList[i];
+        }
+        newShirtList[-1] = shirt;
+        return newShirtList;
+    }
+}
