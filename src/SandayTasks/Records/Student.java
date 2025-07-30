cb0110259b60322811f73106396a5eb9e86e070b
tomer-hershman <lili.hershman@gmail.com>
2025-07-30 11:46:45 +0300

    tomer-records,added info printer to student record

--- src/SandayTasks/Records/Student.java
+++ src/SandayTasks/Records/Student.java
@@ -1,5 +1,10 @@
 package SandayTasks.Records;
 
-public record Student (String name, int grade, char clas){
+public record Student(String name, int grade, char clas) {
+    public void printStudentInfo() {
+        System.out.print(name);
+        System.out.print(" is in grade " + grade);
+        System.out.println(", in class " + clas);
+    }
 
 }
