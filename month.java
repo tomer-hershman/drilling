6347fa290e1ffe401f9241a9aec0a69cbfaafb1c
tomer-hershman <lili.hershman@gmail.com>
2025-07-30 17:30:36 +0300

    tomer- enum, added months

--- month.java
+++ month.java
@@ -1,2 +1,21 @@
 public enum month {
+    JANUARY(30),
+    FEBRUARY(28),
+    MARCH(31),
+    APRIL(30),
+    MAY(31),
+    JUNE(30),
+    JULY(31),
+    AUGUST(31),
+    SEPTEMBER(30),
+    OCTOBER(31),
+    NOVEMBER(30),
+    DECEMBER(31);
+
+
+    private final int days;
+
+    month(int days) {
+        this.days = days;
+    }
 }
