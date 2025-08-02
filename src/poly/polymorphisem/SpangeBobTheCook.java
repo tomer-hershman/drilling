55644882f0beadd864e5d552c07044edc43b4ff4
tomer-hershman <lili.hershman@gmail.com>
2025-08-02 10:39:35 +0300

    tomer- poly, added cook spangebob

---
+++ src/poly/polymorphisem/SpangeBobTheCook.java
@@ -0,0 +1,7 @@
+package poly.polymorphisem;
+
+public class SpangeBobTheCook extends SpangeBob{
+    public SpangeBobTheCook(int age, String name, String favoriteFood) {
+        super(age, name, favoriteFood);
+    }
+}
