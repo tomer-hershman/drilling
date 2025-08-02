08f9e818d47e6a8a2b2f6f8182a6ee7c851254e3
tomer-hershman <lili.hershman@gmail.com>
2025-08-02 10:47:12 +0300

    tomer- poly, added og play basketball in spunge cook constractor

--- src/poly/polymorphisem/SpangeBobTheCook.java
+++ src/poly/polymorphisem/SpangeBobTheCook.java
@@ -3,6 +3,7 @@ package poly.polymorphisem;
 public class SpangeBobTheCook extends SpangeBob{
     public SpangeBobTheCook(int age, String name, String favoriteFood) {
         super(age, name, favoriteFood);
+        super.playBasketball();
     }
 
     @Override
