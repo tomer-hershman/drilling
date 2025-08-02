83516d8626caa647a8180605e0fd8db6b2e52192
tomer-hershman <lili.hershman@gmail.com>
2025-08-02 12:31:37 +0300

    tomer- poly, finished quetions

--- src/poly/polymorphisem/PolyHelperClass.java
+++ src/poly/polymorphisem/PolyHelperClass.java
@@ -1,5 +1,7 @@
 package poly.polymorphisem;
 
+import java.util.Arrays;
+
 public class PolyHelperClass {
     public static void basketballPlayerAgenda(BasketballPlayer[] players) {
         for (BasketballPlayer player : players) {
@@ -9,5 +11,22 @@ public class PolyHelperClass {
             }
             player.playBasketball();
         }
+    }
+
+    public static void printAgendaForBasketBallFish(Fish[] fish) {
+        int BasketBallFishCount = 0;
+        for (Fish f : fish) {
+            if (f instanceof BasketballPlayer) {
+                BasketBallFishCount++;
+            }
+        }
+        BasketballPlayer[] basketballPlayers = new BasketballPlayer[BasketBallFishCount];
+        for (int i = 0, j = 0; i < fish.length; i++) {
+            if (fish[i] instanceof BasketballPlayer) {
+                basketballPlayers[j++] = (BasketballPlayer) fish[i];
+            }
+
+        }
+        basketballPlayerAgenda(basketballPlayers);
     }
 }
